f399055196aa27ecef07c4c8491a2d0e079d2906
noisrucer <mmmo@mmmos-MacBook-Air.local>
2024-11-20 14:28:41 +0800

    shared prediction function

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 
-def main():
+def test_predict(img_path: str) -> int:
+    '''Read the image specified by img_path and predict an integer label'''
     pass
 
-if __name__ == '__main__':
-    main()
